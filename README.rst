c1ea150b0d898946799b4a35cc6c19702fb992db
Drew Peterson <peterson.drew@gmail.com>
2012-06-15 12:20:59 -0500

    Switched to aria2, removed OSX support  (as a result of ditching virtualbox), switched from virtualbox to kvm

--- README.rst
+++ README.rst
@@ -1,57 +1,43 @@
 Overview
 ========
 
-Microsoft provides virtual machine disk images to facilitate website testing 
-in multiple versions of IE, regardless of the host operating system. 
+Microsoft provides virtual machine disk images to facilitate website testing
+in multiple versions of IE, regardless of the host operating system.
 Unfortunately, setting these virtual machines up without Microsoft's VirtualPC
-can be extremely difficult. The ievms scripts aim to facilitate that process using
-VirtualBox on Linux or OS X. With a single command, you can have IE6, IE7, IE8
+can be extremely difficult. The kvm-ievms scripts aim to facilitate that process using
+KVM on Linux. With a single command, you can have IE6, IE7, IE8
 and IE9 running in separate virtual machines.
-
-.. image:: http://pledgie.com/campaigns/15995.png?skin_name=chrome
-   :alt: Click here to lend your support to ievms and make a donation at pledgie.com!
-   :target: http://pledgie.com/campaigns/15995
 
 
 Requirements
 ============
 
-* VirtualBox (http://virtualbox.org)
-* Curl (Ubuntu: ``sudo apt-get install curl``)
-* Linux Only: unrar (Ubuntu: ``sudo apt-get install unrar``)
+* KVM (Debian/Ubuntu: ``sudo apt-get install kvm``)
+* aria2 (Debian/Ubuntu: ``sudo apt-get install aria2``)
+* Linux Only: unrar (nonfree) (Debian/Ubuntu: ``sudo apt-get install unrar``)
 * Patience
 
 
 Installation
 ============
 
-1. Install VirtualBox.
+1. Install KVM.
 
 2. Download and unpack ievms:
 
    * Install IE versions 6, 7, 8 and 9.
 
-         curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | bash
+         curl -s https://raw.github.com/petersondrew/ievms/master/ievms.sh | bash
 
    * Install specific IE versions (IE7 and IE9 only for example):
 
-         curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | IEVMS_VERSIONS="7 9" bash
+         curl -s https://raw.github.com/petersondrew/ievms/master/ievms.sh | IEVMS_VERSIONS="7 9" bash
 
-3. Launch Virtual Box.
+3. Connect to your virtual machines via vnc.
 
-4. Choose ievms image from Virtual Box.
-
-5. Install VirtualBox Guest Additions (pre-mounted as CD image in the VM).
-
-6. **IE6 only** - Install network adapter drivers by opening the ``drivers`` CD image in the VM.
-
-.. note:: The IE6 network drivers *must* be installed upon first boot, or an
-   activation loop will prevent subsequent logins forever. If this happens, 
-   restoring to the ``clean`` snapshot will reset the activation lock.
-
-The VHD archives are massive and can take hours or tens of minutes to 
+The VHD archives are massive and can take hours or tens of minutes to
 download, depending on the speed of your internet connection. You might want
-to start the install and then go catch a movie, or maybe dinner, or both. 
+to start the install and then go catch a movie, or maybe dinner, or both.
 
 Once available and started in VirtualBox, the password for ALL VMs is "Password1".
 
@@ -71,16 +57,16 @@ Specifying the install path
 
 To specify where the VMs are installed, use the INSTALL_PATH variable:
 
-    curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | INSTALL_PATH="/Path/to/.ievms" bash
+    curl -s https://raw.github.com/petersondrew/ievms/master/ievms.sh | INSTALL_PATH="/Path/to/.ievms" bash
 
 
 Passing additional options to curl
 ----------------------------------
 
-The ``curl`` command is passed any options present in the ``CURL_OPTS`` 
+The ``aria2`` command is passed any options present in the ``ARIA_OPTS`` 
 environment variable. For example, you can set a download speed limit:
 
-    curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | CURL_OPTS="--limit-rate 50k" bash
+    curl -s https://raw.github.com/petersondrew/ievms/master/ievms.sh | CURL_OPTS="--limit-rate 50k" bash
 
 
 Features
@@ -88,20 +74,20 @@ Features
 
 Clean Snapshot
     A snapshot is automatically taken upon install, allowing rollback to the
-    pristine virtual environment configuration. Anything can go wrong in 
+    pristine virtual environment configuration. Anything can go wrong in
     Windows and rather than having to worry about maintaining a stable VM,
-    you can simply revert to the ``clean`` snapshot to reset your VM to the
+    you can simply revert to the first snapshot to reset your VM to the
     initial state.
 
     The VMs provided by Microsoft will not pass the Windows Genuine Advantage
     and cannot be activated. Unfortunately for us, that means our VMs will
-    lock us out after 30 days of unactivated use. By reverting to the 
-    ``clean`` snapshot the countdown to the activation apocalypse is reset,
+    lock us out after 30 days of unactivated use. By reverting to the
+    clean snapshot the countdown to the activation apocalypse is reset,
     effectively allowing your VM to work indefinitely.
 
 
 Resuming Downloads
-    If one of the comically large files fails to download, the ``curl`` 
+    If one of the comically large files fails to download, the ``aria2`` 
     command used will automatically attempt to resume where it left off. 
     Thanks, rcmachado (https://github.com/rcmachado).
 
--- ievms.sh
+++ ievms.sh
@@ -6,233 +6,140 @@ set -o errtrace
 set -o errexit
 set -o pipefail
 
-curl_opts=${CURL_OPTS:-""}
+aria_opts=${ARIA_OPTS:-""}
 
 log()  { printf "$*\n" ; return $? ;  }
 
 fail() { log "\nERROR: $*\n" ; exit 1 ; }
 
 create_home() {
-    def_ievms_home="${HOME}/.ievms"
-    ievms_home=${INSTALL_PATH:-$def_ievms_home}
+  def_ievms_home="${HOME}/.ievms"
+  ievms_home=${INSTALL_PATH:-$def_ievms_home}
 
-    mkdir -p "${ievms_home}"
-    cd "${ievms_home}"
+  mkdir -p "${ievms_home}"
+  cd "${ievms_home}"
 }
 
 check_system() {
-    # Check for supported system
-    kernel=`uname -s`
-    case $kernel in
-        Darwin|Linux) ;;
-        *) fail "Sorry, $kernel is not supported." ;;
-    esac
+  # Check for supported system
+  kernel=`uname -s`
+  case $kernel in
+    Linux) ;;
+    *) fail "Sorry, $kernel is not supported." ;;
+  esac
 }
 
-check_virtualbox() {
-    log "Checking for VirtualBox"
-    hash VBoxManage 2>&- || fail "VirtualBox is not installed! (http://virtualbox.org)"
-
-    log "Checking for Oracle VM VirtualBox Extension Pack"
-    if ! VBoxManage list extpacks | grep "Oracle VM VirtualBox Extension Pack"
-    then
-        version=`VBoxManage -v`
-        ext_version="${version/r/-}"
-        short_version="${version/r*/}"
-        url="http://download.virtualbox.org/virtualbox/${short_version}/Oracle_VM_VirtualBox_Extension_Pack-${ext_version}.vbox-extpack"
-        archive="Oracle_VM_VirtualBox_Extension_Pack-${ext_version}.vbox-extpack"
-
-        log "Downloading Oracle VM VirtualBox Extension Pack from ${url} to ${ievms_home}/${archive}"
-        if ! curl ${curl_opts} -C - -L "${url}" -o "${archive}"
-        then
-            fail "Failed to download ${url} to ${ievms_home}/${archive} using 'curl', error code ($?)"
-        fi
-
-        log "Installing Oracle VM VirtualBox Extension Pack from ${ievms_home}/${archive}"
-        if ! VBoxManage extpack install "${archive}"
-        then
-            fail "Failed to install Oracle VM VirtualBox Extension Pack from ${ievms_home}/${archive}, error code ($?)"
-        fi
-    fi
-}
-
-install_unrar() {
-    case $kernel in
-        Darwin) download_unrar ;;
-        Linux) fail "Linux support requires unrar (sudo apt-get install for Ubuntu/Debian)" ;;
-    esac
-}
-
-download_unrar() {
-    url="http://www.rarlab.com/rar/rarosx-4.0.1.tar.gz"
-    archive="rar.tar.gz"
-
-    log "Downloading unrar from ${url} to ${ievms_home}/${archive}"
-    if ! curl ${curl_opts} -C - -L "${url}" -o "${archive}"
-    then
-        fail "Failed to download ${url} to ${ievms_home}/${archive} using 'curl', error code ($?)"
-    fi
-
-    if ! tar zxf "${archive}" -C "${ievms_home}/" --no-same-owner
-    then
-        fail "Failed to extract ${ievms_home}/${archive} to ${ievms_home}/," \
-            "tar command returned error code $?"
-    fi
-
-    hash unrar 2>&- || fail "Could not find unrar in ${ievms_home}/rar/"
+check_kvm() {
+  log "Checking for KVM/libvirt-bin/virsh"
+  which kvm 1>&- 2>&- || fail "kvm is not installed"
+  which virt-install 1>&- 2>&- || fail "libvirt-bin (virt-install) is not installed"
+  which virsh 1>&- 2>&- || fail "virsh is not installed"
 }
 
 check_unrar() {
-    PATH="${PATH}:${ievms_home}/rar"
-    hash unrar 2>&- || install_unrar
+  PATH="${PATH}:${ievms_home}/rar"
+  which unrar 1>&- 2>&- || fail "unrar is not installed (unrar-free will not work either)"
+}
+
+check_aria() {
+ log "Checking for aria2"
+ which aria2 1>&- 2>&- || fail "aria2 is not installed"
 }
 
 build_ievm() {
-    case $1 in
-        6) 
-            urls="http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_XP_IE6.exe"
-            vhd="Windows XP.vhd"
-            vm_type="WindowsXP"
-            ;;
-        7) 
-            urls=`echo http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_Vista_IE7.part0{1.exe,2.rar,3.rar,4.rar,5.rar,6.rar}`
-            vhd="Windows Vista.vhd"
-            vm_type="WindowsVista"
-            ;;
-        8) 
-            urls=`echo http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_7_IE8.part0{1.exe,2.rar,3.rar,4.rar}`
-            vhd="Win7_IE8.vhd"
-            vm_type="Windows7"
-            ;;
-        9) 
-            urls=`echo http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_7_IE9.part0{1.exe,2.rar,3.rar,4.rar,5.rar,6.rar,7.rar}`
-            vhd="Windows 7.vhd"
-            vm_type="Windows7"
-            ;;
-        *)
-            fail "Invalid IE version: ${1}"
-            ;;
-    esac
+  case $1 in
+    6)
+      urls="http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_XP_IE6.exe"
+      vhd="Windows XP.vhd"
+      os_variant="winxp"
+      ;;
+    7)
+      urls=`echo http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_Vista_IE7.part0{1.exe,2.rar,3.rar,4.rar,5.rar,6.rar}`
+      vhd="Windows Vista.vhd"
+      os_variant="vista"
+      ;;
+    8)
+      urls=`echo http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_7_IE8.part0{1.exe,2.rar,3.rar,4.rar}`
+      vhd="Win7_IE8.vhd"
+      os_variant="win7"
+      ;;
+    9)
+      urls=`echo http://download.microsoft.com/download/B/7/2/B72085AE-0F04-4C6F-9182-BF1EE90F5273/Windows_7_IE9.part0{1.exe,2.rar,3.rar,4.rar,5.rar,6.rar,7.rar}`
+      vhd="Windows 7.vhd"
+      os_variant="win7"
+      ;;
+    *)
+      fail "Invalid IE version: ${1}"
+      ;;
+  esac
 
-    vm="IE${1}"
-    vhd_path="${ievms_home}/vhd/${vm}"
-    mkdir -p "${vhd_path}"
-    cd "${vhd_path}"
+  vm="IE${1}"
+  vhd_path="${ievms_home}/vhd/${vm}"
+  mkdir -p "${vhd_path}"
+  cd "${vhd_path}"
 
-    log "Checking for existing VHD at ${vhd_path}/${vhd}"
-    if [[ ! -f "${vhd}" ]]
+  log "Checking for existing VHD at ${vhd_path}/${vhd}"
+  if [[ ! -f "${vhd}" ]]
+  then
+
+    log "Checking for downloaded VHDs at ${vhd_path}/"
+    for url in $urls
+    do
+      archive=`basename $url`
+      log "Downloading VHD from ${url} to ${ievms_home}/"
+      #if ! curl ${curl_opts} -C - -L -O "${url}"
+      if ! aria2 ${aria_opts} -c -d ${ievms_home} "${url}"
+      then
+        fail "Failed to download ${url} to ${vhd_path}/ using 'aria2', error code ($?)"
+      fi
+    done
+
+    rm -f "${vhd_path}/"*.vmc
+
+    log "Extracting VHD from ${vhd_path}/${archive}"
+    if ! unrar e -y "${archive}"
     then
+      fail "Failed to extract ${archive} to ${vhd_path}/${vhd}," \
+        "unrar command returned error code $?"
+    fi
+  fi
 
-        log "Checking for downloaded VHDs at ${vhd_path}/"
-        for url in $urls
-        do
-            archive=`basename $url`
-            log "Downloading VHD from ${url} to ${ievms_home}/"
-            if ! curl ${curl_opts} -C - -L -O "${url}"
-            then
-                fail "Failed to download ${url} to ${vhd_path}/ using 'curl', error code ($?)"
-            fi
-        done
+  log "Checking for existing ${vm} VM"
+  if ! virsh dominfo "${vm}" 2>&-
+  then
 
-        rm -f "${vhd_path}/"*.vmc
-
-        log "Extracting VHD from ${vhd_path}/${archive}"
-        if ! unrar e -y "${archive}"
-        then
-            fail "Failed to extract ${archive} to ${vhd_path}/${vhd}," \
-                "unrar command returned error code $?"
-        fi
+    virtio_url="https://alt.fedoraproject.org/pub/alt/virtio-win/latest/images/bin/virtio-win-0.1-22.iso"
+    virtio_iso=`basename $virtio_url`
+    log "Downloading latest VirtIO drivers from ${virtio_url}"
+    if ! aria2 -c -d ${ievms_home} "${virtio_url}"
+    then
+      fail "Failed to download "${virtio_url}" to ${ievms_home}/ using 'aria2', error code ($?)"
     fi
 
-    log "Checking for existing ${vm} VM"
-    if ! VBoxManage showvminfo "${vm}"
-    then
-
-        case $kernel in
-            Darwin) ga_iso="/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso" ;;
-            Linux) ga_iso="/usr/share/virtualbox/VBoxGuestAdditions.iso" ;;
-        esac
-
-        log "Creating ${vm} VM"
-        VBoxManage createvm --name "${vm}" --ostype "${vm_type}" --register
-        VBoxManage modifyvm "${vm}" --memory 256 --vram 32
-        VBoxManage storagectl "${vm}" --name "IDE Controller" --add ide --controller PIIX4 --bootable on
-        VBoxManage storagectl "${vm}" --name "Floppy Controller" --add floppy
-        VBoxManage internalcommands sethduuid "${vhd_path}/${vhd}"
-        VBoxManage storageattach "${vm}" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium "${vhd_path}/${vhd}"
-        VBoxManage storageattach "${vm}" --storagectl "IDE Controller" --port 0 --device 1 --type dvddrive --medium "${ga_iso}"
-        VBoxManage storageattach "${vm}" --storagectl "Floppy Controller" --port 0 --device 0 --type fdd --medium emptydrive
-        declare -F "build_ievm_ie${1}" && "build_ievm_ie${1}"
-        VBoxManage snapshot "${vm}" take clean --description "The initial VM state"
-    fi
+    log "Creating ${vm} VM"
+    virt-install -n "${vm}" --import --hvm --os-type=windows --os-variant=${os_variant} -r 256 \
+      --disk "${vhd_path}/${vhd}",device=disk,bus=ide \
+      -c "${virtio_iso}" \
+      --network bridge=br0 \
+      --graphics vnc,listen=0.0.0.0 --noautoconsole
+    virsh snapshot-create "${vm}"
+    virst start "${vm}"
+  fi
 
 }
 
-build_ievm_ie6() {
-    log "Setting up ${vm} VM"
-
-    if [[ ! -f "${ievms_home}/drivers/PRO2KXP.exe" ]]
-    then
-        download_driver "http://downloadmirror.intel.com/8659/eng/PRO2KXP.exe" "Downloading 82540EM network adapter driver"
-
-        if [[ ! -f "${ievms_home}/drivers/autorun.inf" ]]
-        then
-            cd "${ievms_home}/drivers"
-            echo '[autorun]' > autorun.inf
-            echo 'open=PRO2KXP.exe' >> autorun.inf
-            cd "${ievms_home}"
-        fi
-    fi
-
-    log "Changing network adapter to 82540EM"
-    VBoxManage modifyvm "${vm}" --nictype1 "82540EM"
-
-    build_and_attach_drivers
-}
-
-download_driver() {
-    if [[ ! -d "${ievms_home}/drivers" ]]
-    then
-        mkdir -p "${ievms_home}/drivers"
-    fi
-
-    log $2
-
-    cd "${ievms_home}/drivers"
-    # Currently the IE6 driver download server doesn't support resume
-    if ! curl ${curl_opts} -L -O "$1"
-    then
-        fail "Failed to download $1 to ${ievms_home}/drivers/ using 'curl', error code ($?)"
-    fi
-    cd ..
-}
-
-build_and_attach_drivers() {
-    log "Building drivers ISO for ${vm}"
-    if [[ ! -f "${ievms_home}/drivers.iso" ]]
-    then
-      log "Writing drivers ISO"
-
-      
-      case $kernel in
-          Darwin) hdiutil makehybrid "${ievms_home}/drivers" -o "${ievms_home}/drivers.iso" ;;
-          Linux) mkisofs -o "${ievms_home}/drivers.iso" "${ievms_home}/drivers" ;;
-      esac
-    fi
-
-    VBoxManage storageattach "${vm}" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "${ievms_home}/drivers.iso"
-}
 
 check_system
 create_home
-check_virtualbox
+check_kvm
+check_aria
 check_unrar
 
 all_versions="6 7 8 9"
 for ver in ${IEVMS_VERSIONS:-$all_versions}
 do
-    log "Building IE${ver} VM"
-    build_ievm $ver
+  log "Building IE${ver} VM"
+  build_ievm $ver
 done
 
 log "Done!"
